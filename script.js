39bfd7626b6291a132b6a46ecccdb96d4cbc6541
Adam <adammiller.2000nj@gmail.com>
2023-09-16 13:49:35 -0400

    added modal & small changes to .amount buttons

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const body = document.querySelector('body');
 const lifeline5050 = document.getElementById('5050-lifeline');
 const setSwapBtn = document.getElementById('setSwap-btn');
 const questionBox = document.querySelector('.question');
-const audio = new Audio("media/background.mp3");
+const audioBackground = new Audio("media/background.mp3");
+const audioCorrect = new Audio('media/correct.mp3');
+const audioWrong = new Audio('media/wrong.mp3');
 const modalBtn = document.getElementById("modalBtn");
 const moneyContainer = document.querySelectorAll('.amount').forEach(el => el.addEventListener('click', selectedQuestion))
 const lifelineContainer = document.querySelectorAll('.lifeline').forEach(el => el.addEventListener('click', selectedLifeline))
@@ -11,11 +13,19 @@ const correctAnswer = document.querySelectorAll('.answer-container > p').forEach
 
 function selectedQuestion(){
     this.classList.add('selected');
-    audio.play();
+    audioBackground.play();
+    audioBackground.loop = true;
+    document.querySelectorAll('.amount').forEach(el => el.style.zIndex = '-3');
     document.querySelectorAll('p > span').forEach(el => {
         el.style.animation = 'fadeInAnimation 3s ease-in-out';
     });
-    new Audio('media/question.mp3').play();
+    this.style.zIndex = '';
+    this.removeAttribute('onclick');
+    if(this.classList.contains('amount')){
+        new Audio('media/question.mp3').play();
+        this.classList.remove('amount');
+        this.classList.add('amount-current');
+    }
 }
 
 function selectedLifeline(){
@@ -26,17 +36,17 @@ function selectedLifeline(){
 function selectedAnswer() {
     this.classList.add('answer-selected');
     this.classList.remove('answer');
-    audio.pause();
+    audioBackground.pause();
 
     if(this.classList.contains('correct')) {
         this.style.backgroundImage = 'radial-gradient(rgb(51, 208, 27), rgb(27, 129, 11))';
-        new Audio('media/correct.mp3').play();
+        audioCorrect.play();
         document.querySelectorAll('p > span').forEach(el => {
             el.style.animation = '';
         });
     } else {
         this.style.backgroundImage = 'radial-gradient(rgb(208, 27, 27), rgb(129, 11, 11))';
-        new Audio('media/wrong.mp3').play();
+        audioWrong.play();
         document.querySelectorAll('p > span').forEach(el => {
             el.style.animation = '';
         });
@@ -51,15 +61,20 @@ function selectedAnswer() {
 
     this.style.animation = 'fadeInAnimation 3s ease-in-out';
     questionBox.style.animation = '';
+    document.querySelectorAll('.amount').forEach(el => el.style.zIndex = '');
 };
 
+// 50/50 Lifeline button
 lifeline5050.addEventListener('click', () => {
     const answerList = document.querySelectorAll('.answer-container > .half');
     answerList.forEach(element => element.style.opacity = 0);
     answerList.forEach(element => element.style.zIndex = '-3');
 });
 
+// Button to close modal
 modalBtn.addEventListener('click', () => {
-    audio.play();
+    audioBackground.play();
+    audioBackground.loop = true;
+    document.querySelectorAll('.amount').forEach(el => el.style.zIndex = '-3');
     document.getElementById('modal-container').style.display = 'none';
-})
+});
