b7f9fa8a29b47a5be796eb6b34098850eba642e6
Adam Miller <adammiller.2000nj@gmail.com>
2025-01-09 00:36:37 -0500

    cabin v2 questions

--- questions.js
+++ questions.js
@@ -71,47 +71,47 @@ function answerReset(){
 };
 
 function questionOne() {
-    question(`What item is used to breed cows?`, `Seeds`, `Wheat`, `Carrots`, `Apples`, 2, 1, 3);
+    question(`What is the name of the boys' groupchat?`, `Tim's Bathroom`, `Jason Fanclub`, `Penis Meeting`, `Money Town`, 2, 1, 3);
 };
 function questionTwo() {
-    question(`What is the name of Minecraft's original main character?`, `Steve`, `Alex`, `Bryan`, `Notch`, 1, 3, 4);
+    question(`Who was the artist behind the masterpiece "Ruan Forrester"?`, `Jason`, `Andrew`, `Tim`, `Adam`, 1, 3, 4);
 };
 function questionThree() {
-    question(`Which of these Minecraft parodies did CaptainSparklez not make?`, `TNT`, `Revenge`, `Don't Mine at Night`, `Minecraft Style`, 3, 1, 2);
+    question(`Which of these is the name of a wrestler on HKW?`, `Pogo Joe`, `Mr. Baby`, `King Raven`, `Wheelin Dealin Ian`, 3, 1, 2);
 };
 function questionFour() {
-    question(`Which of these enchantments is NOT for tools?`, `Fortune`, `Looting`, `Efficiency`, `Unbreaking`, 2, 4, 3);
+    question(`What state is the Super Smash Con tournament hosted in?`, `New Jersey`, `Virginia`, `New York`, `Maryland`, 2, 4, 3);
 };
 function questionFive() {
-    question(`Which of these items does a zombie NOT drop?`, `Rotten Flesh`, `Baked Potato`, `Iron Shovel`, `Golden Carrot`, 4, 1, 3);
+    question(`What year was the groups first trip to Mount Tammany?`, `2018`, `2019`, `2020`, `2017`, 4, 1, 3);
 };
 function questionSix() {
-    question(`What country is Mojang located in?`, `Germany`, `United States`, `Europe`, `Sweden`, 4, 1, 2);
+    question(`Which of these towns has Tim not had an address?`, `Toms River`, `Bayville`, `Beachwood`, `Lacey`, 4, 1, 2);
 };
 function questionSeven() {
-    question(`What is Notch's real name?`, `John Minecraft`, `Markus Persson`, `Todd Howard`, `Peder Bergensten`, 2, 1, 3);
+    question(`Which of these slasher killers is Tim's favorite?`, `The Joker`, `Jason Voorhees`, `Michael Myers`, `Ghostface`, 2, 1, 3);
 };
 function questionEight() {
-    question(`Which of these mobs can drop coal when slain?`, `Wandering Traders`, `Husks`, `Wither Skeletons`, `Blazes`, 3, 1, 4);
+    question(`Who won the most points from RFNTNOTN Season One?`, `Ethan`, `Andrew`, `Adam`, `Tim`, 3, 1, 4);
 };
 function questionNine() {
-    question(`What is the minimal amount of obsidian blocks needed to create a nether portal?`, `8`, `12`, `10`, `9`, 3, 4, 2);
+    question(`How did the boys celebrate Zak's 21st birthday?`, `Tim's House`, `Applebee's`, `Karaoke`, `Bowling`, 3, 4, 2);
 };
 function questionTen() {
-    question(`What item does a creeper drop when killed by a skeleton?`, `Creeper Head`, `TNT`, `Music Disc`, `Iron Ingot`, 3, 2, 4);
+    question(`Which of these animals was a former pet of Adam's?`, `Fish`, `Raccoon`, `Squirrel`, `Parrot`, 3, 2, 4);
 };
 function questionEleven() {
-    question(`In which biome does emerald ore spawn?`, `Plains`, `Mountains`, `Badlands`, `Savannah`, 2, 1, 4);
+    question(`Which one of these isn't the name of one of Zak's friends?`, `Bird`, `Bobo`, `Weirdo`, `Frodo`, 2, 1, 4);
 };
 function questionTwelve() {
-    question(`What Minecraft achievement do you get by acquiring diamonds?`, `Cover Me In Diamonds!`, `Diamonds to you!`, `DIAMONDS!`, `OH, SHINY!`, 3, 1, 4);
+    question(`What was the name of HKW's first PPV episode?`, `Lightning Road`, `Collision Course`, `Thunderstruck`, `Wrestle Time`, 3, 1, 4);
 };
 function questionThirteen() {
-    question(`What year did Minecraft officially release?`, `2010`, `2009`, `2012`, `2011`, 4, 3, 1);
+    question(`Which of these usernames/tags has not been used by Ryan?`, `Micro`, `Ratio`, `Spraze`, `Blaze`, 4, 3, 1);
 };
 function questionFourteen() {
-    question(`What is considered the rarest item in Minecraft?`, `Dragon Egg`, `Nether Star`, `Dragon Head`, `Enchanted Golden Apple`, 1, 3, 4);
+    question(`Which season was Tim's family featured on 'The Supernanny'?`, `Season 7`, `Season 8`, `Season 4`, `Season 6`, 1, 3, 4);
 };
 function questionFifteen() {
-    question(`How many peaceful mobs are currently in Minecraft?`, `32`, `34`, `28`, `30`, 1, 3, 4);
+    question(`Which of these people appeared in Adam's first Minecraft video?`, `Jason`, `David`, `Tim`, `Keon`, 1, 3, 4);
 };
